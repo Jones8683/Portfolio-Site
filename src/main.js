fec6281ce8ed00aeb6e265acc3020d5e225d2a2d
Jones8683 <outerrim1977@gmail.com>
2026-02-17 14:47:21 +1030

    Make favicon automatically round to allow for easy changing

--- src/main.js
+++ src/main.js
@@ -3,6 +3,30 @@ import App from "./App.vue";
 import router from "./router";
 import "./assets/main.css";
 
+const link =
+  document.querySelector("link[rel*='icon']") || document.createElement("link");
+link.rel = "icon";
+document.head.appendChild(link);
+
+const img = new Image();
+img.onload = () => {
+  const size = Math.min(img.width, img.height);
+  const canvas = document.createElement("canvas");
+  canvas.width = canvas.height = size;
+
+  const ctx = canvas.getContext("2d");
+  ctx.beginPath();
+  ctx.arc(size / 2, size / 2, size / 2, 0, Math.PI * 2);
+  ctx.clip();
+
+  ctx.drawImage(img, (size - img.width) / 2, (size - img.height) / 2);
+  link.href = canvas.toDataURL();
+};
+if (link.href) {
+  img.src = link.href;
+  img.crossOrigin = "Anonymous";
+}
+
 const app = createApp(App);
 
 app.use(router);
