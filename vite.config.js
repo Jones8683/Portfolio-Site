f06c3f9f6d0708f85024f2f0b6821078458e2946
Jones8683 <outerrim1977@gmail.com>
2026-03-23 13:54:11 +1030

    Update CSS configuration

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,13 @@ import vue from "@vitejs/plugin-vue";
 export default defineConfig({
   plugins: [vueDevTools(), vue()],
   css: {
-    transformer: "postcss",
+    lightningcss: {
+      targets: {
+        chrome: 112 << 16,
+        firefox: 113 << 16,
+        safari: (16 << 16) | (4 << 8),
+      },
+    },
   },
   resolve: {
     alias: {
