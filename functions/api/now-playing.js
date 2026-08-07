58a6edf45e936c7e8fb169fa85e9c358c9353346
Jones Jankovic <outerrim1977@gmail.com>
2026-08-07 17:37:32 +0930

    Hide all artists except first one

--- functions/api/now-playing.js
+++ functions/api/now-playing.js
@@ -33,7 +33,7 @@ function trackPayload(item) {
   return {
     isPlaying: true,
     title: item.name,
-    artist: item.artists.map((a) => a.name).join(", "),
+    artist: item.artists[0]?.name ?? "",
     album: item.album?.name ?? null,
     albumArt: item.album?.images?.[0]?.url ?? null,
     songUrl: item.external_urls?.spotify ?? null,
